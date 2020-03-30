af6f538a6c4c2fb25aaad1de72a83168b1b71d1c
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-30 20:02:45 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,6 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-      return new_array.length
-    end
+    self.split(/[.!?]+/).count
+  end
 end
